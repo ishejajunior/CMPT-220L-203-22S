ee3b3914c1bfc9b15b5d3d91771487e17ff2d1b8
ishejajunior <rickyjunior.isheja1@marist.edu>
2022-03-04 04:35:26 +0200

    Create Plant.java

---
+++ JavaLabs/Lab 6/src/base/Plant.java
@@ -0,0 +1,22 @@
+package base;
+
+
+public class Plant {
+    public int watering;
+    public String name;
+    public String fam_name;
+
+static void wateringperday(int watering,String name){
+    System.out.println(name+"must be watered"+watering+"perday");
+    
+    }
+
+
+static void sunlight(int watering,String name){
+    int sun;
+    sun = watering * 7;
+    System.out.println( name +"must be put on sunlight not warmer than"+ sun+"celcius");
+
+    }
+}
+
