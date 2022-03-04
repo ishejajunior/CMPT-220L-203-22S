57679434f3fb54205a402a74ec8cc0deb4c541e5
ishejajunior <rickyjunior.isheja1@marist.edu>
2022-03-04 04:35:39 +0200

    Update Main.java

--- JavaLabs/Lab 6/src/base/Main.java
+++ JavaLabs/Lab 6/src/base/Main.java
@@ -1,32 +1,56 @@
 package base;
 
+import java.util.*;
 public class Main {
-    /**
-     * # Lab Six
-     * ---
-     *
-     * For this lab we're going to be messing around with our Linear Data Types,
-     * as well as creating a plant Object!
-     *
-     * Our Goals are:
-     * - Creating an Array
-     * - Creating an ArrayList
-     * - Creating an LinkedList
-     * - Creating an Queue
-     * - Creating an Stack
-     * - Create a Plant
-     *
-     * Please show the ability to:
-     * - Create these data types
-     * - Populate the data types
-     * - Print out the Linear Data Types
-     */
     public static void main(String[] args) {
-        // Create an Array, Populate it, and Print it out
-        // Create an ArrayList, Populate it, and Print it out
-        // Create an LinkedList, Populate it, and Print it out
-        // Create an Queue, Populate it, and Print it out
-        // Create an Stack, Populate it, and Print it out
+        String[] names = {"Ricky", "Isheja", "Junior"};
+
+        System.out.println("Printing array:");
+         for (int i = 0; i < names.length; i++){
+         System.out.println(names[i]);
+        }// Create an Array, Populate it, and Print it out
+
+
+        ArrayList<String> names1 = new ArrayList<>();
+        names1.add("ricky");
+        names1.add("junior");
+        names1.add("Isheja");
+        System.out.println(names1);// Create an ArrayList, Populate it, and Print it out
+        
+        LinkedList<String> iphone = new LinkedList<String>();
+        iphone.add("Iphone4");
+        iphone.add("Iphone5");
+        iphone.add("Iphone6");
+        iphone.add("Iphone7");
+        iphone.add("Iphone8");
+        iphone.add("Iphone9");
+        iphone.add("IphoneX");
+        System.out.println(iphone);// Create an LinkedList, Populate it, and Print it out
+
+        Queue<String> bankline = new LinkedList<String>();
+        bankline.add("customer1");
+        bankline.add("customer2");
+        bankline.add("customer3");
+        bankline.add("customer4");
+        System.out.println(bankline);// Create an Queue, Populate it, and Print it out
+
+        Stack<String> plates = new Stack<String>();
+        plates.push("plate1");
+        plates.push("plate2");
+        plates.push("plate3");
+        plates.push("plate4");
+        System.out.println(plates);// Create an Stack, Populate it, and Print it out
+
         // Using the information in the Lab 6 Jupyter Notebook create an instance of plant here.
+        Plant moss = new Plant();
+        moss.fam_name="caleb";
+        moss.name="MOSS";
+        moss.watering='5';
+        System.out.println(moss);
+        System.out.println("Printing the objects:");
+        for (int a = 0; a < 3; a++){
+        System.out.println(moss);
+       }
+        
     }
 }
