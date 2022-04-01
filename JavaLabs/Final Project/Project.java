9c758c535b7c558f3528dec763d7d21853281632
ishejajunior <rickyjunior.isheja1@marist.edu>
2022-04-01 04:41:13 +0200

    Create Project.java

---
+++ JavaLabs/Final Project/Project.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class project{
+    public treenode root;
+
+    private class treenode{
+        private treenode left;
+        private treenode right;
+        private int date;
+
+        public treenode(int data){
+            this.data= data;
+        }
+    }
+    public void createbinarytree(){
+        treenode first= new treenode(1);
+        treenode second=new treenode(2);
+        treenode third=new treenode(3);
+        
+    }
+}
