09abf37ab13557172f410bad9d27f9c38575ae63
ishejajunior <rickyjunior.isheja1@marist.edu>
2022-04-01 04:41:34 +0200

    Create Listitem.java

---
+++ JavaLabs/Lab7/src/base/Listitem.java
@@ -0,0 +1,13 @@
+package base;
+
+
+public class Listitem {
+    public int listitem1;// This is a normal Variable
+    public Listitem listitem2;//this is an object that chains the list
+    Listitem(int listitem1, int listeitem2){
+        listitem1= Newlistitem1;
+        listeitem2=Newlistitem2;
+    }
+
+}
+
