5daa6ad6ef9e352eb0069314154f751ece218bc0
ishejajunior <rickyjunior.isheja1@marist.edu>
2022-04-01 04:41:27 +0200

    Create Main.java

---
+++ JavaLabs/Lab7/src/base/Main.java
@@ -0,0 +1,11 @@
+Package base;
+import java.util.*;
+public class Main {
+    Listitem obj1=new Listitem(2,null);
+    Listitem obj2=new Listitem(3,obj1);
+    Listitem obj3=new Listitem(4,obj2);
+    Listitem obj4=new Listitem(5,obj3);
+    Listitem obj5=new Listitem(6,obj4);
+system.out.println();
+
+}
