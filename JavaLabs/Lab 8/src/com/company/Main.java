a8bb0ceb3acaf19f17a524c61d2336f2d8aec454
ishejajunior <rickyjunior.isheja1@marist.edu>
2022-04-15 03:03:03 +0200

    Create Main.java

---
+++ JavaLabs/Lab 8/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        T.addbotom(1);
+        T.addbotom(2);
+        T.addbotom(3);
+        T.addbotom(4);
+        T.addbotom(5);
+        T.output();
+    }
+}
